25b3246652764e9250e309e2474e4aea624f9fd3
GlebSmirnov11V <Hearthgleb1@gmail.com>
2020-09-14 07:40:06 +0000

    Перестановка местами чисел

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
-
-int main() {
-  std::cout << "Hello World!\n";
+ 
+using namespace std;
+int main()
+{
+    int x;
+    int y; 
+    cout << "VVedite chisla" <<endl;
+    cin >> x;
+    cin >> y;
+    x = x + y;
+    y = x - y;
+    x = x - y;
+    cout << " x=" << x << " y=" << y << endl;
+    return 0;
 }
